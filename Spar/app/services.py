f0097399bafcf40278b8c56001160f24f195627e
Jonyg8910 <113592164+Jonyg8910@users.noreply.github.com>
2023-09-26 20:17:33 +0200

    Add files via upload

---
+++ Spar/app/services.py
@@ -0,0 +1,14 @@
+# app/services.py
+
+def save_money_for_workout(user):
+    savings_per_workout = 50  # A fixed amount for each workout
+
+    # Ensure the user has enough in their bank account
+    if user.bank_balance < savings_per_workout:
+        return False
+
+    # Transfer the savings from the bank account to the savings account
+    user.bank_balance -= savings_per_workout
+    user.savings_balance += savings_per_workout
+
+    return user.savings_balance
